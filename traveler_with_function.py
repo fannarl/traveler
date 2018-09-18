d78f4c5d896632ff9ebefc6e0cae314b8e0f37e8
fannarl <fannarleo95@hotmail.com>
2018-09-18 20:53:53 +0000

    rnd changes

--- traveler_with_function.py
+++ traveler_with_function.py
@@ -13,8 +13,6 @@
 # Til að fara austur eða vestur þarf að bæta 1 við player breytuna til að fara austur eða -1 til að fara vestur #
 # Þegar player er = 3 þá vinnur hann                                                                            #
 #################################################################################################################
-
-
 player = 1
 valid_direction = 'nsew'
 direction = ''
@@ -24,30 +22,35 @@ east = 1
 west = 1
 victory = False
 
+#meðan direction er ekki í valid_direction halda áfram að byðja um direction 
 def not_valid(p_dir, v_dir):
     while p_dir not in v_dir:
         print("Not a valid direction!")
         p_dir = input("Direction: ").lower()
         return p_dir, v_dir
 
-def dir_func(inp_dir, player_int):
-        if inp_dir == "n":
-            player_int = player_int + north
-        elif inp_dir == "s":
-            player_int = player_int - south
-        elif inp_dir == "e":
-            player_int = player_int + east
-        elif inp_dir == "w":
-            player_int = player_int - west
+def dir_func(a_dir, a_player):
+        for a_dir in valid_direction:
+            if a_dir == "n":
+                a_player = a_player + 3
+                print("n")
+            elif a_dir == "s":
+                a_player = a_player - 3       
+                print("s")     
+            elif a_dir == "e":
+                a_player = a_player + 1
+                print("e")
+            elif a_dir == "w":
+                a_player = a_player - 1  
+                print("w") 
+            return a_player                       
 
 while not victory:
     if player == 1 or player == 2:
         valid_direction = 'n'
         print("You can travel: (N)orth.")
         direction = input("Direction: " ).lower()
-        #meðan direction er ekki í valid_direction halda áfram að byðja um direction              
-        not_valid(player, valid_direction)
-        dir_func(direction, player)
+                     
                     
     elif player == 4:
         valid_direction='nse'
@@ -55,53 +58,43 @@ while not victory:
         direction = input("Direction: " ).lower()
         #checkar á valid direction breytuni og skoðar hvort
         #inputið sé rétt
-        not_valid(player, valid_direction)
-        dir_func(direction, player)
 
     elif player == 5:
         valid_direction = 'ws'
         print("You can travel: (S)outh or (W)est.")
         direction = input("Direction: " ).lower()
-
-        not_valid(player, valid_direction)
-        dir_func(direction, player)       
+    
 
     elif player == 7:
         valid_direction = 'es'
         print("You can travel: (E)ast or (S)outh.")
         direction = input("Direction: " ).lower()
 
-        not_valid(player, valid_direction)
-        dir_func(direction, player)
 
     elif player == 8:
         valid_direction = 'we'
         print("You can travel: (E)ast or (W)est.")
         direction = input("Direction: " ).lower()
 
-        not_valid(player, valid_direction)
-        dir_func(direction, player)
 
     elif player == 9:
         valid_direction = "ws"
         print("You can travel: (S)outh or (W)est.")
         direction = input("Direction: ").lower()
 
-        not_valid(player, valid_direction)
-        dir_func(direction, player)
 
     elif player == 6:
         valid_direction = 'ns'
         print("You can travel: (N)orth or (S)outh.")
         direction = input("Direction: ").lower()
 
-        not_valid(player, valid_direction)
-        dir_func(direction, player)
 
     elif player == 3:
         print("Victory!")
         victory = True
-
+        break
+    not_valid(direction, valid_direction)
+    player = dir_func(direction, player)
 
             
     
