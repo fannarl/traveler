0dd22be96ccacea54e9c6cbcf136bc5d132d0198
fannarl <fannarleo95@hotmail.com>
2018-09-18 15:29:25 +0000

    Bug fixes and comment adding

--- traveler_no_func.py
+++ traveler_no_func.py
@@ -1,4 +1,16 @@
 # Leikur sem lætur þig ferðast um á grid með að nota höfuðáttirnar.
+# Gridið eru 9 reytir sem þýðir að hægt er að ýminda sér það sem beina línu
+#                1, 2, 3, 4, 5, 6, 7, 8, 9
+#Þá myndi gridið líta svona út:
+#               7   8   9
+#               4   5   6
+#               1   2   3
+# Notandi byrjar á 1 og getur ferðast norður
+# Til að fara norður eða suður þarf að bæta 3 við player breytuna til að fara norður og - 3 til að fara suður
+# Til að fara austur eða vestur þarf að bæta 1 við player breytuna til að fara austur eða -1 til að fara vestur
+# Þegar player er = 3 þá vinnur hann
+############################################################################
+
 
 player = 1
 grid = 9
@@ -25,7 +37,7 @@ while not victory:
                     
     elif player == 4:
         valid_direction='nse'
-        print("You can travel: (N)orth or (S)outh or (E)ast.")
+        print("You can travel: (N)orth or (E)ast or (S)outh.")
         direction = input("Direction: " ).lower()
         #checkar á valid direction breytuni og skoðar hvort
         #inputið sé rétt
@@ -41,7 +53,7 @@ while not victory:
 
     elif player == 5:
         valid_direction = 'ws'
-        print("You can travel: (W)est or (S)outh.")
+        print("You can travel: (S)outh or (W)est.")
         direction = input("Direction: " ).lower()
         while direction not in valid_direction:
             print("Not a valid direction!")
@@ -53,7 +65,7 @@ while not victory:
 
     elif player == 7:
         valid_direction = 'es'
-        print("You can travel: (S)outh or (E)ast.")
+        print("You can travel: (E)ast or (S)outh.")
         direction = input("Direction: " ).lower()
         while direction not in valid_direction:
             print("Not a valid direction!")
@@ -65,7 +77,7 @@ while not victory:
 
     elif player == 8:
         valid_direction = 'we'
-        print("You can travel: (W)est or (E)ast.")
+        print("You can travel: (E)ast or (W)est.")
         direction = input("Direction: " ).lower()
         while direction not in valid_direction:
             print("Not a valid direction!")
@@ -77,7 +89,7 @@ while not victory:
 
     elif player == 9:
         valid_direction = "ws"
-        print("You can travel: (W)est or (S)outh.")
+        print("You can travel: (S)outh or (W)est.")
         direction = input("Direction: ").lower()
         while direction not in valid_direction:
             print("Not a valid direction!")
