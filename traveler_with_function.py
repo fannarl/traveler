14fc48e44fb8c26b515489b23de23d7a46840589
fannarl <fannarleo95@hotmail.com>
2018-09-19 22:54:40 +0000

    Final Version, bugs fixed and all comments added

--- traveler_with_function.py
+++ traveler_with_function.py
@@ -25,80 +25,70 @@ victory = False
 #meðan direction er ekki í valid_direction halda áfram að byðja um direction 
 def not_valid(p_dir, v_dir):
     if p_dir not in v_dir:
-        #print("Not a valid direction!")
-        p_dir = input("Direction: ").lower()
         return True
     return False
+    
     
 def dir_func(a_dir, a_player):
     if a_dir == "n":
         a_player = a_player + 3
-        print("n")
-        print(player)
     elif a_dir == "s":
         a_player = a_player - 3       
-        print("s")     
-        print(player)
     elif a_dir == "e":
         a_player = a_player + 1
-        print("e")
-        print(player)
     elif a_dir == "w":
         a_player = a_player - 1  
-        print("w") 
-        print(player)
     return a_player                       
 
 while not victory:
     if player == 1 or player == 2:
         valid_direction = 'n'
         print("You can travel: (N)orth.")
-        direction = input("Direction: " ).lower()
-                     
+        direction = input("Direction: " ).lower()      
                     
     elif player == 4:
         valid_direction='nse'
         print("You can travel: (N)orth or (E)ast or (S)outh.")
         direction = input("Direction: " ).lower()
-        #checkar á valid direction breytuni og skoðar hvort
-        #inputið sé rétt
 
-    elif player == 5:
+    elif player == 5 or player == 9:
         valid_direction = 'ws'
         print("You can travel: (S)outh or (W)est.")
-        direction = input("Direction: " ).lower()
-    
+        direction = input("Direction: " ).lower()    
 
     elif player == 7:
         valid_direction = 'es'
         print("You can travel: (E)ast or (S)outh.")
         direction = input("Direction: " ).lower()
 
-
     elif player == 8:
         valid_direction = 'we'
         print("You can travel: (E)ast or (W)est.")
         direction = input("Direction: " ).lower()
-
-
-    elif player == 9:
-        valid_direction = "ws"
-        print("You can travel: (S)outh or (W)est.")
-        direction = input("Direction: ").lower()
-
 
     elif player == 6:
         valid_direction = 'ns'
         print("You can travel: (N)orth or (S)outh.")
         direction = input("Direction: ").lower()
 
-
     elif player == 3:
         print("Victory!")
         victory = True
         break
 
-    if not not_valid(direction, valid_direction):
-        player = dir_func(direction, player)
+    if  not_valid(direction, valid_direction):
+        print("Not a valid direction!")
+        direction = input("Direction: ").lower()
+        if not not_valid(direction, valid_direction):
+            player = dir_func(direction, player)
     else:
-        print("Not a valid direction!")   
+        player = dir_func(direction, player)
+
+# 1) The First solution was a bit easier since i didnt have to think about how i would implement the functions
+# but after I figured it out it became clear that once you understand how to implement them it would be considerbly easier in the long run 
+
+# 2) The second implementation is way more readable than the first since it is considirebly cleaner and not cluttered in while loops and the main
+# body of the code only contains conditional statements not the actual algorithm.
+
+# 3) In the latter implementation i could contain all the calculations that are used depending on the user input in one place instead of having to put them in
+# individually for each conditional. That saves time and avoids pontential confusion and errors
